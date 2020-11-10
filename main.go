d290b7317c00b827913f7595bcad44659a0a6bdb
eshu0 <willsawesometoolkit@gmail.com>
2020-11-10 05:40:03 +0000

    Update main.go

--- main.go
+++ main.go
@@ -3,48 +3,17 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
-	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
 
-	sl "github.com/eshu0/simplelogger"
 	yaft "github.com/eshu0/yaft/pkg"
 )
 
 func main() {
-	filename := flag.String("logfile", "yaft.log", "Filename out - defaults to yaft.log")
-	session := flag.String("sessionid", "123", "Session - defaults to 123")
-	dbname := flag.String("db", "./yaft.db", "Database defaults to ./yaft.db")
-	inputdir := flag.String("path", "", "")
-	cache := flag.String("cache", "", "")
-	list := flag.String("list", "", "")
-	clear := flag.String("clear", "", "")
-	dupes := flag.String("dupes", "", "")
-	dupeids := flag.String("dupeids", "", "")
-	limit := flag.Int("limit", -1, "")
-	savecsv := flag.String("savecsv", "", "")
-	hashid := flag.Int("hashid", -1, "")
-	filetofind := flag.String("file", "", "File to find")
-	deleteifexists := flag.Bool("die", false, "Delete if exists")
-	yestoall := flag.Bool("yta", false, "Delete if exists")
 
-	flag.Parse()
-
-	savetocsav := false
-
-	if savecsv != nil && *savecsv != "" {
-		savetocsav = true
-	}
-
-	slog := sl.NewSimpleLogger(*filename, *session)
-
-	// lets open a flie log using the session
-	slog.OpenAllChannels()
-
-	fds := &yaft.DataStorage{}
-	fds.Filename = *dbname
-	fds.Create(slog)
+	yapp := YAFTApp{}
+	yapp.Create()
 
 	if filetofind != nil && *filetofind != "" {
 		reader := bufio.NewReader(os.Stdin)
