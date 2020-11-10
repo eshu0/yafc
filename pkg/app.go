f0f5b215aef121b71ce07d47c5f86e75498d0cef
eshu0 <willsawesometoolkit@gmail.com>
2020-11-10 05:52:11 +0000

    Update app.go

--- pkg/app.go
+++ pkg/app.go
@@ -10,34 +10,62 @@ type YAFTApp struct {
 	sl.AppLogger
 	FDS *DataStorage
 	Savetocsv bool
+	//filename *string
+	//session *string
+	dbname *string
+	inputdir *string
+	cache *string
+	list *string
+	clear *string
+	dupes *string
+	dupeids *string
+	Limit *int
+	Hashid *int
+	Filetofind *string
+	Deleteifexists *bool
+	Yestoall bool	
 }
 
-func (yapp *YAFTApp) Create() {
-	filename := flag.String("logfile", "yaft.log", "Filename out - defaults to yaft.log")
-	session := flag.String("sessionid", "123", "Session - defaults to 123")
-	dbname := flag.String("db", "./yaft.db", "Database defaults to ./yaft.db")
-	inputdir := flag.String("path", "", "")
-	cache := flag.String("cache", "", "")
-	list := flag.String("list", "", "")
-	clear := flag.String("clear", "", "")
-	dupes := flag.String("dupes", "", "")
-	dupeids := flag.String("dupeids", "", "")
-	limit := flag.Int("limit", -1, "")
-	savecsv := flag.String("savecsv", "", "")
-	hashid := flag.Int("hashid", -1, "")
-	filetofind := flag.String("file", "", "File to find")
-	deleteifexists := flag.Bool("die", false, "Delete if exists")
-	yapp.Yestoall := flag.Bool("yta", false, "Delete if exists")
+func (yapp *YAFTApp) ParseFlags() {
+	
+	//yapp.filename := flag.String("logfile", "yaft.log", "Filename out - defaults to yaft.log")
+	//yapp.session := flag.String("sessionid", "123", "Session - defaults to 123")
 
-	flag.Parse()
+	yapp.dbname := flag.String("db", "./yaft.db", "Database defaults to ./yaft.db")
+	yapp.nputdir := flag.String("path", "", "")
+	yapp.cache := flag.String("cache", "", "")
+	yapp.list := flag.String("list", "", "")
+	yapp.clear := flag.String("clear", "", "")
+	yapp.dupes := flag.String("dupes", "", "")
+	yapp.dupeids := flag.String("dupeids", "", "")
+	yapp.Limit := flag.Int("limit", -1, "")
+	savecsv := flag.String("savecsv", "", "")
+	yapp.Hashid := flag.Int("hashid", -1, "")
+	yapp.Filetofind := flag.String("file", "", "File to find")
+	yapp.Deleteifexists := flag.Bool("die", false, "Delete if exists")
+	yestoall := flag.Bool("yta", false, "Delete if exists")
 
 	yapp.Savetocsv = false
 
 	if savecsv != nil && *savecsv != "" {
-		yapp.Savetocsv = true
+		yapp.Savecsv = true
 	}
 
+	
+	yapp.Yestoall = false
+	if yestoall != nil {
+		yapp.Yestoall = *yestoall
+	}
+
+
+	flag.Parse()
+
+}
+
+func (yapp *YAFTApp) Create() {
+
+
 	yapp.FDS := &DataStorage{}
-	yapp.FDS.Filename = *dbname
+	yapp.FDS.Filename = *yapp.dbname
 	yapp.FDS.Create(slog)
 }
