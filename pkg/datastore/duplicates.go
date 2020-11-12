3addbc680dbe4d283ee2005dd2432eaca05d9dac
eshu0 <willsawesometoolkit@gmail.com>
2020-11-12 20:41:00 +0000

    Update duplicates.go

--- pkg/datastore/duplicates.go
+++ pkg/datastore/duplicates.go
@@ -2,37 +2,13 @@ package datastore
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 
-	sli "github.com/eshu0/simplelogger/pkg/interfaces"
 	"github.com/eshu0/yaft/pkg/models"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-type HashIdnCount struct {
-	HashId int64
-	Count  int64
-}
-
-func SaveDuplicates(FilePath string, Log sli.ISimpleLogger, hd map[string][]*models.HashRelationship) bool {
-	bytes, err1 := json.MarshalIndent(hd, "", "\t") //json.Marshal(p)
-	if err1 != nil {
-		Log.LogErrorf("SaveDuplicates()", "Marshal json for %s failed with %s ", FilePath, err1.Error())
-		return false
-	}
-
-	err2 := ioutil.WriteFile(FilePath, bytes, 0644)
-	if err2 != nil {
-		Log.LogErrorf("SaveDuplicates()", "Saving %s failed with %s ", FilePath, err2.Error())
-		return false
-	}
-
-	return true
-}
-
-func (fds *Storage) GetDuplicateHashIds(limit int) []HashIdnCount {
+func (fds *Storage) GetDuplicateHashIds(limit int) []models.HashIdnCount {
 	var rows *sql.Rows
 
 	if limit < 0 {
@@ -59,7 +35,7 @@ func (fds *Storage) GetDuplicateHashes(limit int) map[string][]*models.HashRelat
 	return results
 }
 
-func (fds *Storage) ParseDuplicatedHashIDsRows(rows *sql.Rows) []HashIdnCount {
+func (fds *Storage) ParseDuplicatedHashIDsRows(rows *sql.Rows) []models.HashIdnCount {
 	var hashid int64
 	var count int64
 
