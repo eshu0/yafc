8f20f410d956914d39b6f33df86f83bc1083daeb
eshu0 <willsawesometoolkit@gmail.com>
2020-11-12 07:45:18 +0000

    Update main.go

--- main.go
+++ main.go
@@ -9,14 +9,11 @@ func main() {
 	yapp := yaft.YAFTApp{}
 
 	// parse the input flags
-	yapp.LogInfo("Parsing Flags")
 	yapp.ParseFlags()
 
 	// create the database
-	yapp.LogInfo("Creating Database")
 	yapp.Create()
 
-	yapp.LogInfo("Processing")
 	yapp.Process()
 
 	yapp.FinishLogging()
