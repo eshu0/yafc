a91156c8f65e39e1a104d76686d0da14869bf0ae
eshu0 <willsawesometoolkit@gmail.com>
2020-11-12 20:40:53 +0000

    Update hashidncount.go

--- pkg/models/hashidncount.go
+++ pkg/models/hashidncount.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 
 	sli "github.com/eshu0/simplelogger/pkg/interfaces"
+	"github.com/eshu0/yaft/pkg/models"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -13,7 +14,7 @@ type HashIdnCount struct {
 	Count  int64
 }
 
-func SaveDuplicates(FilePath string, Log sli.ISimpleLogger, hd map[string][]*HashRelationship) bool {
+func SaveDuplicates(FilePath string, Log sli.ISimpleLogger, hd map[string][]*models.HashRelationship) bool {
 	bytes, err1 := json.MarshalIndent(hd, "", "\t") //json.Marshal(p)
 	if err1 != nil {
 		Log.LogErrorf("SaveDuplicates()", "Marshal json for %s failed with %s ", FilePath, err1.Error())
